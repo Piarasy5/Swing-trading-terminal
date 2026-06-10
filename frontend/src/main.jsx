349fc27d6b5f86e9144d0f5fbce022c05d01189d
Piaras <mcaffnta@gmail.com>
2026-06-10 12:58:35 +1000

    fix frontend structure - add src folder

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SwingTerminal from "./SwingTerminal.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SwingTerminal />
+  </React.StrictMode>
+);
